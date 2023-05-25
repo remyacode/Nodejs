521692fb0e2b597b63d86eb1d620a698a2796f3b
Remya C <remyachandrasekhar710@gmail.com>
2023-05-26 01:44:32 +0530

    OurName Server

---
+++ NodeAppTut/app.js
@@ -0,0 +1,21 @@
+/*
+//task
+const http = require('http');
+
+
+const server = http.createServer((req,res)=>{
+    console.log(req);
+});
+
+server.listen(3000);
+*/
+//DELIVERABLE
+
+const http = require('http');
+
+
+const server = http.createServer((req,res)=>{
+    console.log('Remya C');
+});
+
+server.listen(4000);
--- tut/play.js
+++ tut/play.js
@@ -35,7 +35,7 @@ for(let hobby of hobbies){
 console.log(hobbies.map(hobby=>{return 'Hobby'+hobby}));
 console.log(hobbies);
 
-*/
+
 //task 3.1
 const hobbies =['apple', 'oranges' , '', 'mango', '' , 'lemon'];
 
@@ -46,3 +46,125 @@ console.log(hobbies.map(hobby=>{
                     return hobby}
             
     }));
+
+    const obj1 = {'key1': 1, "key2": 2, "key3": 1000}
+
+//task 4
+//1
+const obj1 = {'key1': 1, "key2": 2, "key3": 1000}
+
+const { key1, key3} = { ...obj1}
+
+console.log(key1, key3)
+
+        //output: 1 1000
+    
+//2
+const arr1 = ['value1', 'value2']
+
+const [ val1, val2 ] = arr1
+
+
+
+console.log(val1)
+
+console.log(val2)
+
+        //output: value1
+                  value2
+
+//3
+
+const obj1 = {'key1': 1, "key2": 2, "key3": 1000}
+
+let { key1, key3} = obj1
+
+
+
+key1 = 20;
+
+key3 = 123
+
+console.log(obj1.key1, obj1.key3)
+
+        //output: 1 1000
+
+
+//task 5
+
+//1
+console.log('a');
+
+console.log('b');
+
+setTimeout(() => console.log('c'), 3000)
+
+console.log('d');
+
+        //output: 
+        a
+        b
+        d
+        c 
+
+
+//2
+console.log('a');
+
+console.log('b');
+
+setTimeout(() => console.log('c'), 3000)
+
+setTimeout(() => console.log('d'), 0)
+
+console.log('e');
+
+    //output:
+    a 
+    b 
+    e 
+    d 
+    c 
+
+
+
+//3
+const late = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+
+
+const S = async () => {
+
+ console.log('a');
+
+ console.log('b');
+
+
+
+ await late(3000);
+
+
+
+ console.log('c');
+
+ console.log('d');
+
+
+
+ console.log('e');
+
+};
+
+
+
+S();
+
+        //output::
+        a 
+        b 
+        c 
+        d 
+        e 
+
+*/
+
