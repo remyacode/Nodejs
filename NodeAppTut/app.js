fd115134bcb27e0d1281d22918de6aa39446cfeb
Remya C <remyachandrasekhar710@gmail.com>
2023-05-26 15:41:04 +0530

    code clean

--- NodeAppTut/app.js
+++ NodeAppTut/app.js
@@ -13,39 +13,15 @@ server.listen(3000);
 
 const http=require('http');
 
-const fs=require('fs');
-const server = http.createServer((req,res)=>{
+const routes=require('/home/remya-c/Desktop/Sharpener Tasks/Node.js/NodeAppTut/route.js');
+
+//
+const server = http.createServer(routes);
+//const server = http.createServer(routes.handler);
     //console.log('Remya C');
-    if(req.url==='/'){
-        //res.setHeader('Content-type','text/html');
-        res.write('<html><head><title>Enter Message</title></head><body><form action="/message" method="POST">');
-        res.write(fs.readFileSync('message.txt'))
-        res.write('<br><input type="text" name="message"><button type="submit">Send</button></form></body></html>');
-        return res.end();
-    }
-    if(req.url==="/message" && req.method==="POST"){
-        const body=[];
-        req.on('data',(chunk)=>{
-            console.log(chunk)
-            body.push(chunk);
-        });
-        return req.on('end',()=>{
-            const parsedBody=Buffer.concat(body).toString();
-            const message=parsedBody.split('=')[1];
-            fs.writeFile('message.txt',message,err=>{
-                res.statusCode=302;
-                res.setHeader('Location','/');
-                //res.write(fs.readFile('message.txt'))
-                return res.end();
-            });
-           
-        });
-        
-        
-    }
-        res.setHeader('Content-type','text/html');
-        res.write('<html><head><title>MYfirst Page</title></head><body><h1>Hi from Nodejs</h1></body></html>');
-         res.end();
+
+
+server.listen(4001);
     /*
     if(req.url=='/home'){
         //console.log(req.url,req.method,req.header)
@@ -67,6 +43,3 @@ const server = http.createServer((req,res)=>{
     }
     */
     
-});
-
-server.listen(4001);
