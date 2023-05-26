69eb3eea9e4f55b399c27adcfacdb6ba060300a4
Remya C <remyachandrasekhar710@gmail.com>
2023-05-26 12:47:26 +0530

    to diiferent urls

--- NodeAppTut/app.js
+++ NodeAppTut/app.js
@@ -11,11 +11,30 @@ server.listen(3000);
 */
 //DELIVERABLE
 
-const http = require('http');
+const http=require('http');
 
 
 const server = http.createServer((req,res)=>{
-    console.log('Remya C');
+    //console.log('Remya C');
+    if(req.url=='/home'){
+        //console.log(req.url,req.method,req.header)
+        //console.log(req.url);
+        res.setHeader('Content-type','text/html');
+        res.write('<html><head><title>Home</title></head><body><h1>Welcome Home</h1></body></html>');
+        res.end();
+    }
+    
+    if(req.url=='/about'){
+        res.setHeader('Content-type','text/html');
+        res.write('<html><head><title>About</title></head><body><h1>Welcome to About us page</h1></body></html>');
+        res.end();
+    }
+    if(req.url=='/node'){
+        res.setHeader('Content-type','text/html');
+        res.write('<html><head><title>About</title></head><body><h1>Welcome to Node js Project</h1></body></html>');
+        res.end();
+    }
+    
 });
 
-server.listen(4000);
+server.listen(4001);
