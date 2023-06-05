03c57fe6584e95e78996efe783ce96dbb437030a
Remya C <remyachandrasekhar710@gmail.com>
2023-06-05 13:05:40 +0530

    SQL connecting app to server

--- Dynamic Routes/00-starting-setup/controllers/shop.js
+++ Dynamic Routes/00-starting-setup/controllers/shop.js
@@ -29,10 +29,22 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+  Cart.getCart(cart=>{
+    Product.fetchAll(products=>{
+      const cartProducts =[];
+      for(product of products){
+        if(cart.products.find(prod=>prod.id===product.id)){
+            cartProducts.push({productData})
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart'
+      });
+    })
+ 
+  })
+
 };
 
 exports.postCart = (req,res,next)=>{
